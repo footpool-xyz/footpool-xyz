16694c9370a108505f55d1ca0fb70f0f54d4b359
Meke <cryptomeke666@gmail.com>
2024-09-20 17:36:11 +0200

    Try to solve nexjslint

--- packages/nextjs/app/matchweeks/page.tsx
+++ packages/nextjs/app/matchweeks/page.tsx
@@ -3,20 +3,20 @@ import MatchWeekCard from "./_components/MatchWeekCard";
 import type { NextPage } from "next";
 
 const MatchWeekPage: NextPage = () => {
-    const title = "On going Match Weeks";
-    const subtitle = "Compete with Footpool users to find the best bettor and win cash prizes.";
+  const title = "On going Match Weeks";
+  const subtitle = "Compete with Footpool users to find the best bettor and win cash prizes.";
 
-    return (
-        <div className="flex items-center flex-col flex-grow pt-10">
-            <BannerTitle title={title} subtitle={subtitle} />
+  return (
+    <div className="flex items-center flex-col flex-grow pt-10">
+      <BannerTitle title={title} subtitle={subtitle} />
 
-            <div className="flex-grow bg-base-300 w-full mt-16 px-8 py-12">
-                <MatchWeekCard title="MatchWeek 1" season="Season 2024/2025" stakeholdersCount={350} amountIn={15000} />
-                {/* <MatchWeekCard /> */}
-                {/* <MatchWeekCard /> */}
-            </div>
-        </div>
-    );
+      <div className="flex-grow bg-base-300 w-full mt-16 px-8 py-12">
+        <MatchWeekCard title="MatchWeek 1" season="Season 2024/2025" stakeholdersCount={350} amountIn={15000} />
+        {/* <MatchWeekCard /> */}
+        {/* <MatchWeekCard /> */}
+      </div>
+    </div>
+  );
 };
 
 export default MatchWeekPage;
